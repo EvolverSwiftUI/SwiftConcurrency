6ce2ab802620dcdaebc0a0f61a7edd343ae39395
Sivaram Yadav <sivaramaiah.nakka@concentrix.com>
2022-04-26 22:58:42 +0530

    Initial Commit

---
+++ SwiftConcurrency/ConcurrencyBootcamp.swift
@@ -0,0 +1,36 @@
+//
+//  ConcurrencyBootcamp.swift
+//  SwiftConcurrency
+//
+//  Created by Sivaram Yadav on 4/26/22.
+//
+
+import SwiftUI
+
+// Swift Concurrency [or Writing Asynchronus Code in Swift]:
+/*
+ previously we did concurrency(asynchronus code) using two ways predominently
+    1. Escaping Closures
+    2. Combine
+ */
+
+// Keywords:
+/*
+    1. do
+    2. try
+    3. catch
+    4. throw
+    5. throws
+ */
+
+struct ConcurrencyBootcamp: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ConcurrencyBootcamp_Previews: PreviewProvider {
+    static var previews: some View {
+        ConcurrencyBootcamp()
+    }
+}
---
+++ SwiftConcurrency/SwiftConcurrencyApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftConcurrencyApp.swift
+//  SwiftConcurrency
+//
+//  Created by Sivaram Yadav on 4/26/22.
+//
+
+import SwiftUI
+
+@main
+struct SwiftConcurrencyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
