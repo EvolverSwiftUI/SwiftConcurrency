d76cb5ff5a5d6c9abe288b773f9a0fef35913c54
Sivaram Yadav <sivaramaiah.nakka@concentrix.com>
2022-04-28 09:38:17 +0530

    Added download image using escaping closure syntax on async code way

---
+++ SwiftConcurrency/DownloadImageAsync.swift
@@ -0,0 +1,87 @@
+//
+//  DownloadImageAsync.swift
+//  SwiftConcurrency
+//
+//  Created by Sivaram Yadav on 4/28/22.
+//
+
+import SwiftUI
+
+class DownloadImageAsyncImageLoader {
+    
+    let url = URL(string: "https://picsum.photos/200")!
+    
+    func handleResponse(data: Data?, response: URLResponse?) -> UIImage? {
+        
+        guard
+            let data = data,
+            let image = UIImage(data: data),
+            let response = response as? HTTPURLResponse,
+            response.statusCode >= 200 && response.statusCode < 300 else {
+                return nil
+            }
+        
+        return image
+    }
+    
+    func downloadWithEscaping(completionHandler: @escaping (_ image: UIImage?, _ error: Error?) -> Void) {
+        URLSession.shared.dataTask(with: url) { [weak self] data, response, error in
+            
+            // as below code is reusable so made a separate function and utilized below
+            
+            //guard
+            //    let data = data,
+            //    let image = UIImage(data: data),
+            //    let response = response as? HTTPURLResponse,
+            //    response.statusCode >= 200 && response.statusCode < 300 else {
+            //        completionHandler(nil, error)
+            //        return
+            //    }
+            //completionHandler(image, nil)
+            
+            let image = self?.handleResponse(data: data, response: response)
+            completionHandler(image, error)
+        }
+        .resume()
+    }
+}
+
+class DownloadImageAsyncViewModel: ObservableObject {
+    
+    @Published var image: UIImage? = nil
+    let loader = DownloadImageAsyncImageLoader()
+
+    func fetchImage() {
+//        self.image = UIImage(systemName: "heart.fill")
+        loader.downloadWithEscaping { [weak self] image, error in
+            DispatchQueue.main.async {
+                self?.image = image
+            }
+        }
+    }
+}
+
+struct DownloadImageAsync: View {
+    
+    @StateObject private var viewModel = DownloadImageAsyncViewModel()
+    
+    var body: some View {
+        ZStack {
+            if let image = viewModel.image {
+                Image(uiImage: image)
+                    .resizable()
+                    .scaledToFit()
+                    .frame(width: 250, height: 250)
+            }
+        }
+        .onAppear {
+            viewModel.fetchImage()
+        }
+    }
+}
+
+struct DownloadImageAsync_Previews: PreviewProvider {
+    static var previews: some View {
+        DownloadImageAsync()
+    }
+}
