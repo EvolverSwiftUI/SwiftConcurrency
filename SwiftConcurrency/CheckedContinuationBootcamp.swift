f7162a51b2d3bde75dc7aaa7ce833e438a80b80a
Sivaram Yadav <sivaramaiah.nakka@concentrix.com>
2022-05-16 19:31:12 +0530

    added Checked Continuation to convert old API format to swift concurrency support

---
+++ SwiftConcurrency/CheckedContinuationBootcamp.swift
@@ -0,0 +1,80 @@
+//
+//  CheckedContinuationBootcamp.swift
+//  SwiftConcurrency
+//
+//  Created by Sivaram Yadav on 5/16/22.
+//
+
+import SwiftUI
+
+
+class CheckedContinuationBootcampNetworkManager: ObservableObject {
+    
+    func getData(url: URL) async throws -> Data {
+        do {
+            let (data, _) = try await URLSession.shared.data(from: url, delegate: nil)
+            return data
+        } catch  {
+            throw error
+        }
+    }
+    
+    func getData2(url: URL) async throws -> Data {
+        return try await withCheckedThrowingContinuation { continuation in
+            URLSession.shared.dataTask(with: url) { data, response, error in
+                if let data = data {
+                    continuation.resume(returning: data)
+                } else if let error = error {
+                    continuation.resume(throwing: error)
+                } else {
+                    continuation.resume(throwing: URLError(.badURL))
+                }
+            }
+            .resume()
+        }
+    }
+}
+
+class CheckedContinuationBootcampViewModel: ObservableObject {
+    
+    @Published var image: UIImage? = nil
+    let networkManager = CheckedContinuationBootcampNetworkManager()
+    
+    func getImage() async {
+        guard let url = URL(string: "https://picsum.photos/400") else { return }
+        do {
+            //let data = try await networkManager.getData(url: url)
+            let data = try await networkManager.getData2(url: url)
+            if let image = UIImage(data: data) {
+                self.image = image
+            }
+        } catch  {
+            debugPrint("Error:", error.localizedDescription)
+        }
+    }
+}
+
+struct CheckedContinuationBootcamp: View {
+    
+    @StateObject private var viewModel = CheckedContinuationBootcampViewModel()
+    
+    var body: some View {
+        ZStack {
+            if let image = viewModel.image {
+                Image(uiImage: image)
+                    .resizable()
+                    .scaledToFit()
+                    .frame(width: 200, height: 200)
+            }
+        }
+        .task {
+            await viewModel.getImage()
+        }
+    }
+}
+
+struct CheckedContinuationBootcamp_Previews: PreviewProvider {
+    static var previews: some View {
+        CheckedContinuationBootcamp()
+    }
+}
